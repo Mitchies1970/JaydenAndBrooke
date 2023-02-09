d12e5ed846e013a6e3986cf18c96d13310c64e88
Michael Annesely <michaelannesley@gmail.com>
2023-02-09 16:19:53 +1030

    abc

--- Solution/JnB/ConsoleTests/Program.cs
+++ Solution/JnB/ConsoleTests/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace ConsoleTests
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ConsoleTests
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            static async Task Main(string[] args)
+            {
+
+                IConfiguration Configuration = new ConfigurationBuilder()
+                  .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true)
+                  .AddEnvironmentVariables()
+                  .AddCommandLine(args)
+                  .Build();
+            }
+
         }
     }
 }
